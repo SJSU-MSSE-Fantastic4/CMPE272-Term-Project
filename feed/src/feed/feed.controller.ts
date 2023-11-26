ec2f93b4ae0722cddc8240480b8934917e46c62e
sjkchang <sjkchang@gmail.com>
2023-11-26 13:45:23 -0800

    Combined api docs. Finalized docker compose file. Made small fix to feed service

--- feed/src/feed/feed.controller.ts
+++ feed/src/feed/feed.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, HttpStatus, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+} from '@nestjs/common';
 import { FeedService } from './feed.service';
 import {
   ApiBearerAuth,
@@ -7,7 +13,7 @@ import {
   ApiResponse,
 } from '@nestjs/swagger';
 
-@Controller('')
+@Controller('feed')
 export class FeedController {
   constructor(private readonly feedService: FeedService) {}
 
@@ -26,6 +32,11 @@ export class FeedController {
   })
   async getFeed(@Param('userId') userId: string) {
     let feed = await this.feedService.getFeed(userId);
-    return feed.posts;
+    if (!feed)
+      throw new NotFoundException(
+        'No feed found for user. The user may not exist, or may not be following anyone who has posted.',
+      );
+    if (feed.posts) return feed.posts;
+    else return [];
   }
 }
